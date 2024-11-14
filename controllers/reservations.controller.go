a99d8fb8a32fab25559a5e7f3d9d742cc9159250
Edu Miranda C <eduardo.miranda01@alumnos.ucn.cl>
2024-11-14 00:51:05 -0300

    conexión con mongodb

--- controllers/reservations.controller.go
+++ controllers/reservations.controller.go
@@ -1,7 +1,24 @@
 package controllers
 
 import (
+	"context"
+	"log"
 	m "ms-reservas/models"
+
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func create_res(reservation m.Reservation)
+var mongoClient *mongo.Client
+
+func SetMongoClient(client *mongo.Client) {
+	mongoClient = client
+}
+
+func create_res(reservation m.Reservation) {
+	collection := mongoClient.Database("reservations").Collection("reservations")
+	_, err := collection.InsertOne(context.TODO(), reservation)
+
+	if err != nil {
+		log.Fatalf("Failed to insert reservation: %v", err)
+	}
+}
--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net"
+
+	"ms-reservas/controllers"
+	"ms-reservas/database"
+
+	"google.golang.org/grpc"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	client := database.ConnectMongoDB()
+
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	controllers.SetMongoClient(client)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve gRPC over port: %v", err)
+	}
 }
